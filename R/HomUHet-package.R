6cd97b56e4cf80fe8774b1db675ea548b90ce6e5
Pei-Yang <sephinaca@gmail.com>
2021-02-22 22:49:33 -0500

    change in documenting homuhet-pkg

---
+++ R/HomUHet-package.R
@@ -0,0 +1,12 @@
+#' identify homogeneous and heterogeneous effects of predictors in multiple datasets
+#'
+#' classify the predictors into homogeneous, heterogeneous and unassociated categories. outputs the solution path plots.
+#'
+#' @aliases {HomUHet}-package
+#' HomUHet can be used to identify and separates predictors with homogeneous or heterogeneous effect across multiple data sets
+#' through fitting penalized linear regression models in two steps.
+#' Applicable to Gaussian response variable and very high dimensional data
+#' where the number of predictors could be larger than the number of observations per data set.
+#' @keywords internal
+"_PACKAGE"
+#> [1] "_PACKAGE"
--- R/HomUHet.R
+++ R/HomUHet.R
@@ -1,14 +1,3 @@
-#' identify homogeneous and heterogeneous effects of predictors in multiple datasets
-#'
-#' classify the predictors into homogeneous, heterogeneous and unassociated categories. outputs the solution path plots.
-#'
-#'
-#' HomUHet can be used to identify and separates predictors with homogeneous or heterogeneous effect across multiple data sets
-#' through fitting penalized linear regression models in two steps.
-#' Applicable to Gaussian response variable and very high dimensional data
-#' where the number of predictors could be larger than the number of observations per data set.
-"_PACKAGE"
-#> [1] "_PACKAGE"
 #'
 #' fit a two-step penalized regression model
 #'
